2dc88edc6fc865832d7d18acf887d9cd954fb154
RicardoGomes335 <pb.ricardogomes@gmail.com>
2025-09-14 15:28:35 -0300

    Create trait BelongsToCompany

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\BelongsToCompany;
 use App\Models\Scopes\CompanyScope;
 use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,7 +13,7 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 class Client extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, BelongsToCompany;
 
     protected $fillable = [
         'address_id',
@@ -38,22 +39,5 @@ class Client extends Model
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
-    }
-
-    protected static function booted()
-    {
-        static::addGlobalScope(new CompanyScope);
-
-        static::creating(function ($client) {
-            if (session()->has('company_id')) {
-                $client->company_id = session()->get('company_id');
-            }
-        });
-
-        static::updating(function($client) {
-            if (session()->has('company_id')) {
-                $client->company_id = session()->get('company_id');
-            }
-        });
     }
 }
